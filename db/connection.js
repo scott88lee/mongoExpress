887f53141b57d3ba9d3a6ec7922b96dbb7f58b3d
Scott Lee <scott88lee@gmail.com>
2022-06-01 07:59:25 +0800

    got mongo client sessions working

--- db/connection.js
+++ db/connection.js
@@ -2,22 +2,30 @@ let { MongoClient } = require('mongodb');
 let uri = 'mongodb+srv://test:pwd@homage.cecrjtt.mongodb.net/?retryWrites=true&w=majority'
 
 let mongodb;
+let client;
 
 function connect(callback) {
     MongoClient.connect(uri, (err, db) => {
         if (err) { console.log(err) }
         else {
             console.log('MongoDB connected.')
+            client = db;
             mongodb = db.db('test');
             callback();
         }
     });
 }
+
 function query() {
     return mongodb;
+}
+
+function mongoClient() {
+    return client;
 }
 
 module.exports = {
     connect,
     query,
+    mongoClient
 };
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const db = require('../db/connection')
+const { MongoClient } = require('mongodb')
 
-router.get('/', async (req,res) => {
+router.get('/', async (req, res) => {
     Users = db.query().collection('users')
 
     let result = await Users.find({}).toArray().sort()
@@ -11,14 +12,14 @@ router.get('/', async (req,res) => {
 })
 
 router.get('/insertmany', async (req, res) => {
-    function generatePerson(){
+    function generatePerson() {
         let charset = "ACABABACIDIDIDODO"
         let name = ""
-        for (let i=0; i<8; i++){
-            name += charset[Math.floor(Math.random()*charset.length)]
+        for (let i = 0; i < 8; i++) {
+            name += charset[Math.floor(Math.random() * charset.length)]
         }
-        let age = Math.floor(Math.random()*45) + 10
-        let height = Math.floor(Math.random()*50) + 150
+        let age = Math.floor(Math.random() * 45) + 10
+        let height = Math.floor(Math.random() * 50) + 150
         return {
             name, age, height
         }
@@ -28,20 +29,40 @@ router.get('/insertmany', async (req, res) => {
     doc.push(generatePerson())
     doc.push(generatePerson())
     doc.push(generatePerson())
-    
+
     Users = db.query().collection('users')
     await Users.insertMany(doc)
     res.send(doc)
 })
 
-router.get('/update', async (req,res) => {
+router.get('/update', async (req, res) => {
     Users = db.query().collection('users')
 
-    filter = {name: "Scott"}
-    newData = { $set:{height:180}}
-    let result = await Users.updateOne(filter, newData, {new:true})
+    filter = { name: "Scott" }
+    newData = { $set: { height: 180 } }
+    let result = await Users.updateOne(filter, newData, { new: true })
     console.log(result)
     res.send(result)
+})
+
+router.get('/tx', async (req, res) => {
+    const transactionOptions = {
+        readConcern: { level: 'snapshot' },
+        writeConcern: { w: 'majority' },
+        maxCommitTimeMS: 1000
+    };
+
+    const client = db.mongoClient();
+    const session = client.startSession();
+
+    try {
+        session.startTransaction(transactionOptions);
+    } catch (error) {
+        console.log('Encountered an error during the transaction: ' + error);
+    } finally {
+        await session.endSession();
+        res.send('Done')
+    }
 })
 
 module.exports = router
